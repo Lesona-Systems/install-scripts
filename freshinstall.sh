328c7383783e53a28bf578c86bf349a477a6cf2e
Nicholas Johnson <nejwritings@gmail.com>
2023-04-02 13:53:15 -0500

    add desktop background and condense

--- freshinstall.sh
+++ freshinstall.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 
+mkdir /home/$USER/bin && mkdir /home/$USER/Repos
+
+# update and upgrade
+sudo apt update && sudo apt upgrade -y; 
+
+# Git, initial setup
+sudo apt install git -y; git config --global user.name "Nicholas Johnson" && git config --global user.email "59103181+Lesona-Systems@users.noreply.github.com" && git config --global init.defaultBranch main
+
 # GParted
 sudo apt install gparted -y 
 
